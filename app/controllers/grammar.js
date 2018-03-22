c24c6cb6962327d6a1e7c9b1492855dc05d5c974
Tyler Hasenoehrl <tylrhas@gmail.com>
2018-03-21 21:23:23 -0700

    grammar backend

--- app/controllers/grammar.js
+++ app/controllers/grammar.js
@@ -7,12 +7,25 @@ function check (copy) {
   for (let i = 0; i < copy.length; i++) {
     // break the groups of copy up into words for the spellchecker
     var copyBlock = copy[i]
-    rousseau(copyBlock, function (error, results) {
+    rousseau(copyBlock, {
+      checks: {
+        passive: false,
+        simplicity: false,
+        readibility: false,
+        adverbs: false
+      }
+    }, function (error, results) {
       if (error) {
-
+        console.log(error)
       } else {
         console.log(results)
-        grammar.push(results)
+        for (let i = 0; i < results.length; i++) {
+          var result = {}
+          result.value = results[i].value
+          result.message = results[i].message
+          grammar.push(result)
+        }
+        // grammar.push(results)
       }
     })
   }
